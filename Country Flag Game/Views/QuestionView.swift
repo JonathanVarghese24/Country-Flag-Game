898c0d24b1774487ccc208fb51f274caffd5b9de
Jonathan Varghese <jonathanvarghese24@gmail.com>
2024-01-19 09:39:20 -0600

    done but need to fix image

--- Country Flag Game/Views/QuestionView.swift
+++ Country Flag Game/Views/QuestionView.swift
@@ -6,65 +6,65 @@
 //
 
 import SwiftUI
-
 struct QuestionView: View {
     @EnvironmentObject var gameManager: GameManager
     var body: some View {
         if gameManager.playingGame {
-            VStack(spacing: 20) {
+            VStack (spacing: 20) {
                 HStack {
-                    Text("Country Flag Quiz")
-                        .foregroundColor(.yellow)
-                        .fontWeight(.heavy)
+                    Text ("Country Flag Quiz")
+                        .foregroundColor (.yellow)
+                        .fontWeight (.heavy)
                     Spacer()
-                    Text("\(gameManager.index) out \(gameManager.questions.count)")
+                    Text ("\(gameManager.index) out of \(gameManager.questions.count)")
                         .foregroundColor(.yellow)
                 }
                 ProgressBar(progress: gameManager.progress)
-                VStack(spacing:10) {
-                    Text("Which country's flag is this?")
-                    Image(gameManager.country)
-                        .resizable()
-                        .frame(width:300, height: 200)
-                    ForEach(gameManager.answerChoices) { answer in
+                VStack (spacing: 10) {
+                    Text ("Which country's flag is this?")
+                    Image (gameManager.country)
+                        .resizable ()
+                        .frame (width: 300, height: 200)
+                    ForEach (gameManager.answerChoices) { answer in
                         AnswerRow(answer: answer)
-                            .environmentObject(gameManager)
+                            .environmentObject (gameManager)
                     }
                 }
                 Button {
+                    
                     gameManager.goToNextQuestion()
-                }label: {
+                } label: {
                     CustomButton(text: "Next", background: gameManager.answerSelected ? .yellow : .gray)
                 }
                 .disabled(!gameManager.answerSelected)
+                Spacer ()
             }
-            Spacer()
-                .padding()
-                .background(.cyan)
-                } else {
-                VStack(spacing: 20, content: {
-                    Text("Country Flag Game")
-                        .font(.title)
-                    Text("Congratulations! You have completed the game.")
-                    Text("You scored \(gameManager.score) out of \(gameManager.questions.count)")
-                    Button {
-                        gameManager.reset()
-                    } label : {
-                        CustomButton(text: "Play Again")
-                    }
-                    
-                })
-                foregroundColor(.yellow)
-                    .padding()
-                    .frame(maxWidth: .infinity, maxHeight: .infinity)
-                    .background(.cyan)
-            }
+            .padding()
+            .background(.cyan)
+            
+        }
+        else {
+            VStack(spacing: 20, content: {
+                Text ("Country Flag Game")
+                    .font (.title)
+                Text ("Congratulations! You have completed the game.")
+                Text ("You scored \(gameManager.score) out of \(gameManager.questions.count)")
+                Button {
+                    gameManager.reset()
+                } label: {
+                    CustomButton(text: "Play Again")
+                }
+            })
+            .foregroundColor (.yellow)
+            .padding () .frame (maxWidth: .infinity, maxHeight: .infinity)
+            .background (.cyan)
         }
     }
-
-
-struct QuestionView_Previews: PreviewProvider {
+}
+struct Previews_QuestionView_Previews: PreviewProvider {
     static var previews: some View {
         QuestionView()
     }
 }
+
+
